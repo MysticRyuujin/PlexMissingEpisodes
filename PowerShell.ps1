929cb0951adaa6fc590440c682ea0f30c8e3a9cc
Chase Wright <mysticryuujin@protonmail.com>
2020-04-22 12:35:48 -0500

    Minor Tweaks and Formatting Fixes
    No real functional differences

--- PowerShell.ps1
+++ PowerShell.ps1
@@ -1,7 +1,7 @@
 # TheTVDB Authentication Information
 $TheTVDBAuthentication = @{
-    "apikey" = ""
-    "userkey" = "" # Unique User Key
+    "apikey"   = ""
+    "userkey"  = "" # Unique User Key
     "username" = ""
 }
 
@@ -19,37 +19,39 @@ $IgnoreList = @(
 
 # Ignore Plex Certificate Issues
 if ($PlexServer -match "https") {
-    add-type "using System.Net; using System.Security.Cryptography.X509Certificates; public class TrustAllCertsPolicy : ICertificatePolicy { public bool CheckValidationResult(ServicePoint srvPoint, X509Certificate certificate, WebRequest request, int certificateProblem) { return true; } }"
+    Add-Type "using System.Net; using System.Security.Cryptography.X509Certificates; public class TrustAllCertsPolicy : ICertificatePolicy { public bool CheckValidationResult(ServicePoint srvPoint, X509Certificate certificate, WebRequest request, int certificateProblem) { return true; } }"
     [System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
 }
 		
 # Try to authenticate with TheTVDB API to get a token
 try {
     $TheTVDBToken = (Invoke-RestMethod -Uri "https://api.thetvdb.com/login" -Method Post -Body ($TheTVDBAuthentication | ConvertTo-Json) -ContentType 'application/json').token
-} catch {
+}
+catch {
     Write-Host -ForegroundColor Red "Failed to get TheTVDB API Token:"
     Write-Host -ForegroundColor Red $_
     break
 }
 
 # Create TheTVDB API Headers
-$TVDBHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$TVDBHeaders = [System.Collections.Generic.Dictionary[[String],[String]]]::new()
 $TVDBHeaders.Add("Accept", "application/json")
 $TVDBHeaders.Add("Authorization", "Bearer $TheTVDBToken")
 
 # Create Plex Headers
-$PlexHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
-$PlexHeaders.Add("Authorization","Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$PlexUsername`:$PlexPassword")))")
-$PlexHeaders.Add("X-Plex-Client-Identifier","MissingTVEpisodes")
-$PlexHeaders.Add("X-Plex-Product","PowerShell")
-$PlexHeaders.Add("X-Plex-Version","V1")
+$PlexHeaders = [System.Collections.Generic.Dictionary[[String],[String]]]::new()
+$PlexHeaders.Add("Authorization", "Basic $([System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("$PlexUsername`:$PlexPassword")))")
+$PlexHeaders.Add("X-Plex-Client-Identifier", "MissingTVEpisodes")
+$PlexHeaders.Add("X-Plex-Product", "PowerShell")
+$PlexHeaders.Add("X-Plex-Version", "V1")
 
 # Try to get Plex Token
 try {
     $PlexToken = (Invoke-RestMethod -Uri 'https://plex.tv/users/sign_in.json' -Method Post -Headers $PlexHeaders).user.authToken
-    $PlexHeaders.Add("X-Plex-Token",$PlexToken)
+    $PlexHeaders.Add("X-Plex-Token", $PlexToken)
     [void]$PlexHeaders.Remove("Authorization")
-} catch {
+}
+catch {
     Write-Host -ForegroundColor Red "Failed to get Plex Auth Token:"
     Write-Host -ForegroundColor Red $_
     break
@@ -57,12 +59,14 @@ try {
 
 # Try to get the Library IDs for TV Shows
 try {
-    $TVKeys = ((Invoke-RestMethod -Uri "$PlexServer/library/sections" -Headers $PlexHeaders).MediaContainer.Directory | Where-Object { $_.type -eq "show" }).key
-} catch {
+    $TVKeys = ((Invoke-RestMethod -Uri "$PlexServer/library/sections" -Headers $PlexHeaders).MediaContainer.Directory | Where-Object type -eq "show").key
+}
+catch {
     Write-Host -ForegroundColor Red "Failed to get Plex Library Sections:"
     if ($_.Exception.Response.StatusDescription -eq "Unauthorized") {
-        Write-Host -ForegroundColor Red "Ensure that your source IP is configured under the `"List of IP addresses and networks that are allowed without auth`" setting"
-    } else {
+        Write-Host -ForegroundColor Red "Ensure that your source IP is configured under the 'List of IP addresses and networks that are allowed without auth' setting"
+    }
+    else {
         Write-Host -ForegroundColor Red $_
     }
     break
@@ -81,21 +85,23 @@ ForEach ($TVKey in $TVKeys) {
 $RatingKeys = $RatingKeys | Sort-Object -Unique
 
 # Get all Show Data
-$PlexShows = @{}
+$PlexShows = @{ }
 $Progress = 0
 ForEach ($RatingKey in $RatingKeys) {
     $ShowData = (Invoke-RestMethod -Uri "$PlexServer/library/metadata/$RatingKey/" -Headers $PlexHeaders).MediaContainer.Directory
     $Progress++
     Write-Progress -Activity "Collecting Show Data" -Status $ShowData.title -PercentComplete ($Progress / $RatingKeys.Count * 100)
-    $GUID = $ShowData.guid -replace ".*//(\d+).*",'$1'
+    $GUID = $ShowData.guid -replace ".*//(\d+).*", '$1'
     if ($PlexShows.ContainsKey($GUID)) {
         [void]$PlexShows[$GUID]["ratingKeys"].Add($RatingKey)
-    } else {
-        [void]$PlexShows.Add($GUID,@{
-            "title" = $ShowData.title
-            "ratingKeys" = [System.Collections.Generic.List[int]]::new()
-            "seasons" = @{}
-        })
+    }
+    else {
+        [void]$PlexShows.Add($GUID, @{
+                "title"      = $ShowData.title
+                "ratingKeys" = [System.Collections.Generic.List[int]]::new()
+                "seasons"    = @{ }
+            }
+        )
         [void]$PlexShows[$GUID]["ratingKeys"].Add($ShowData.ratingKey)
     }
 }
@@ -118,15 +124,16 @@ ForEach ($GUID in $PlexShows.Keys) {
                 Write-Host -ForegroundColor Red "Missing parentIndex or index"
                 Write-Host $PlexShows[$GUID]
                 Write-Host $Episode
-            } else {
-                [void]$PlexShows[$GUID]["seasons"][$Episode.parentIndex].Add(@{$Episode.index = $Episode.title})
+            }
+            else {
+                [void]$PlexShows[$GUID]["seasons"][$Episode.parentIndex].Add(@{$Episode.index = $Episode.title })
             }
         }
     }
 }
 
 # Missing Episodes
-$Missing = @{}
+$Missing = @{ }
 $Progress = 0
 ForEach ($GUID in $PlexShows.Keys) {
     $Progress++
@@ -140,9 +147,10 @@ ForEach ($GUID in $PlexShows.Keys) {
             $Results = (Invoke-RestMethod -Uri "https://api.thetvdb.com/series/$GUID/episodes?page=$page" -Headers $TVDBHeaders)
             $Episodes += $Results.data
         }
-    } catch {
+    }
+    catch {
         Write-Warning "Failed to get Episodes for $($PlexShows[$GUID]["title"])"
-	$Episodes = $null
+        $Episodes = $null
     }
     ForEach ($Episode in $Episodes) {
         if (!$Episode.airedSeason) { continue } # Ignore episodes with blank airedSeasons (#11)
@@ -150,25 +158,25 @@ ForEach ($GUID in $PlexShows.Keys) {
         if (!$Episode.firstAired) { continue } # Ignore unaired episodes
         if ((Get-Date).AddDays(-1) -lt (Get-Date $Episode.firstAired)) { continue } # Ignore episodes that aired in the last ~24 hours
         if (!($PlexShows[$GUID]["seasons"][$Episode.airedSeason.ToString()].Values -contains $Episode.episodeName)) {
-	    if (!($PlexShows[$GUID]["seasons"][$Episode.airedSeason.ToString()].Keys -contains $Episode.airedEpisodeNumber)) {
+            if (!($PlexShows[$GUID]["seasons"][$Episode.airedSeason.ToString()].Keys -contains $Episode.airedEpisodeNumber)) {
                 if (!$Missing.ContainsKey($PlexShows[$GUID]["title"])) {
                     $Missing[$PlexShows[$GUID]["title"]] = [System.Collections.Generic.List[hashtable]]::new()
                 }
                 [void]$Missing[$PlexShows[$GUID]["title"]].Add(@{
-                    "airedSeason" = $Episode.airedSeason.ToString()
-                    "airedEpisodeNumber" = $Episode.airedEpisodeNumber.ToString()
-                    "episodeName" = $Episode.episodeName
-                })
-	    }
+                        "airedSeason"        = $Episode.airedSeason.ToString()
+                        "airedEpisodeNumber" = $Episode.airedEpisodeNumber.ToString()
+                        "episodeName"        = $Episode.episodeName
+                    })
+            }
         }
     }
 }
 
 ForEach ($Show in ($Missing.Keys | Sort-Object)) {
     ForEach ($Season in ($Missing[$Show].airedSeason | Sort-Object -Unique)) {
-        $Episodes = $Missing[$Show] | Where-Object { $_.airedSeason -eq $Season }
+        $Episodes = $Missing[$Show] | Where-Object airedSeason -eq $Season
         ForEach ($Episode in $Episodes) {
-            "{0} S{1:00}E{2:00} - {3}" -f $Show,[int]$Season,[int]$Episode.airedEpisodeNumber,$Episode.episodeName
+            "{0} S{1:00}E{2:00} - {3}" -f $Show, [int]$Season, [int]$Episode.airedEpisodeNumber, $Episode.episodeName
         }
     }
 }
